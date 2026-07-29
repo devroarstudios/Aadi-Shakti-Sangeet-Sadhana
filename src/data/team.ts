a1d164df6d5827f3d242b586b0bb84f65039ec8c
Yukta2007 <yuktasonawane2007@gmail.com>
2026-07-29 16:35:32 +0530

    Team page updated

--- src/data/team.ts
+++ src/data/team.ts
@@ -276,4 +276,15 @@ As an integral part of Nrutya-Sadhana School of Dance and Music, she actively co
 Through her leadership, coordination, and passion for administrative excellence, Harpreet continues to support the institution's mission of providing quality education and fostering a positive learning environment.`
 },
 
+{
+slug: "Smt Rekha pandey and Sri Janardan Pandey",
+image: "https://res.cloudinary.com/dcaiszxcb/image/upload/v1785322924/%E0%A4%97%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%B0_%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE_%E0%A4%97%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%B0_%E0%A4%B5%E0%A4%BF%E0%A4%B7%E0%A5%8D%E0%A4%A3%E0%A5%81_%E0%A4%97%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%B0_%E0%A4%A6%E0%A5%87%E0%A4%B5%E0%A5%8B_%E0%A4%AE%E0%A4%B9%E0%A5%87%E0%A4%B6%E0%A5%8D%E0%A4%B5%E0%A4%B0%E0%A4%83_%E0%A4%97%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%83_%E0%A4%B8%E0%A4%BE%E0%A4%95%E0%A5%8D%E0%A4%B7%E0%A4%BE%E0%A4%A4%E0%A5%8D%E0%A4%AA%E0%A4%B0%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE_%E0%A4%A4%E0%A4%B8%E0%A5%8D%E0%A4%AE_20240802_140317_0000_zyyuqs.png",
+name: "Smt Rekha pandey and Sri Janardan Pandey",
+role: "Administration & Management Professional",
+roleHindi: "प्रशासन एवं प्रबंधन विशेषज्ञ",
+bio: "Dedicated administration professional ensuring smooth operations and effective institutional management.",
+video:"https://res.cloudinary.com/dcaiszxcb/image/upload/v1785318290/Bahar_Ulwe_05042025_128.JPG_z3wchh.jpg" ,
+fullBio: `Info`
+},
+
 ];
